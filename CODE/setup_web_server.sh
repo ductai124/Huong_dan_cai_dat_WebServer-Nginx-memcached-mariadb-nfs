2a2fa297711ace1ab351b8780ffae14584288093
Pham Duc Tai <phamductai123456@gmail.com>
2022-01-29 09:37:59 +0700

    KHO CODE

--- CODE/setup_web_server.sh
+++ CODE/setup_web_server.sh
@@ -8,7 +8,7 @@ if systemctl is-active --quiet nginx; then
 fi
 
 
-telnet_output_mariadb="$({ sleep 1; echo $'\e'; } | telnet $ip_server_mariadb 3306 2>&1)"
+telnet_output_mariadb="$({ sleep 1; echo $'\e'; } | telnet $ip_server_mariadb 11211 2>&1)"
 telnet_output_memcached="$({ sleep 1; echo $'\e'; } | telnet $ip_server_memcached 11211 2>&1)"
 telnet_output_nfs_1="$({ sleep 1; echo $'\e'; } | telnet $ip_server_nfs 2049 2>&1)"
 telnet_output_nfs_2="$({ sleep 1; echo $'\e'; } | telnet $ip_server_nfs 20048 2>&1)"
@@ -19,22 +19,25 @@ check_nfs="Connected to $ip_server_nfs"
 i=0
 
 echo "Kiểm tra cổng dịch vụ mariadb trên server có cho phép máy chủ này kết nối không"
+echo ""
 if [[ "$telnet_output_mariadb" == *"$check_maria"* ]]; then
   	echo "Dịch vụ mariadb đã được mở cho máy này trên server, có thể tiến hành cài đặt"
 	else
 	echo "Kiểm tra server đã add source và mở cổng 3306 cho máy web server này chưa do chưa kết nối được dịch vụ mariadb"
 	i=`expr $i + 1`
 fi
-
+echo ""
 echo "Kiểm tra cổng dịch vụ memcached trên server có cho phép máy chủ này kết nối không"
+echo ""
 if [[ "$telnet_output_memcached" == *"$check_memcached"* ]]; then
   	echo "Dịch vụ memcached đã được mở cho máy này trên server, có thể tiến hành cài đặt"
 	else
 	echo "Kiểm tra server đã add source và mở cổng 11211 cho máy web server này chưa do chưa kết nối được dịch vụ memcached"
 	i=`expr $i + 1`
 fi
-
+echo ""
 echo "Kiểm tra cổng dịch vụ NFS trên server có cho phép máy chủ này kết nối không"
+echo ""
 if [[ "$telnet_output_nfs_1" == *"$check_nfs"* ]] && [[ "$telnet_output_nfs_2" == *"$check_nfs"* ]]
 	then
   	echo "Dịch vụ nfs đã được mở cho máy này trên server, có thể tiến hành cài đặt"
@@ -42,14 +45,14 @@ if [[ "$telnet_output_nfs_1" == *"$check_nfs"* ]] && [[ "$telnet_output_nfs_2" =
 	echo "Kiểm tra server đã add source và mở cổng 2049 và 20048 cho máy web server này chưa do chưa kết nối được dịch vụ nfs"
 	i=`expr $i + 1`
 fi
-
-if [ i -gt 0 ]
+echo ""
+if [ $i -gt 0 ]
 	then
 	exit
 	else
 	echo "Đã đủ điểu kiện cài đặt"
 fi
-
+echo ""
 
 echo "Tiếp tục quá trình cài đặt"	
 
