9c73912d0051f319c53bd26297f527cf96ef6afc
ductai124 <52046920+ductai124@users.noreply.github.com>
2022-11-04 18:49:20 +0700

    Update setup_loadbalancing.sh

--- CODE/setup_loadbalancing.sh
+++ CODE/setup_loadbalancing.sh
@@ -58,6 +58,22 @@ echo -e "\t\t }" >> /etc/nginx/nginx.conf
 echo -e "\t }" >> /etc/nginx/nginx.conf
 echo -e "}" >> /etc/nginx/nginx.conf
 
+#Cách này sẽ chỉ xóa các đoạn cần sửa
+#sed -i '37,53d' /etc/nginx/nginx.conf
+#sed -i $'37 i \t upstream backend {' /etc/nginx/nginx.conf
+#sed -i $'38 i \t\t least_conn;' /etc/nginx/nginx.conf
+#sed -i $'39 i \t\t server $ip_web_server_1 max_fails=3 fail_timeout=30 weight=2;' /etc/nginx/nginx.conf
+#sed -i $'40 i \t\t server $ip_web_server_2 max_fails=3 fail_timeout=30 weight=2;' /etc/nginx/nginx.conf
+#sed -i $'41 i \t}' /etc/nginx/nginx.conf
+
+
+#sed -i $'42 i \t server {' /etc/nginx/nginx.conf
+#sed -i $'43 i \t\t listen 80;' /etc/nginx/nginx.conf
+#sed -i $'44 i' /etc/nginx/nginx.conf
+#sed -i $'45 i \t\t location / {' /etc/nginx/nginx.conf
+#sed -i $'46 i \t\t proxy_next_upstream http_404;' /etc/nginx/nginx.conf
+#sed -i $'47 i \t\t proxy_pass http://backend;' /etc/nginx/nginx.conf
+#sed -i $'48 i \t\t }' /etc/nginx/nginx.conf
 
 systemctl enable --now nginx
 systemctl status nginx
